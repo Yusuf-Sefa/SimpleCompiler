79c78b2244ad1ddb30821e8675e3f52613732181
Yusuf-Sefa-Laptop <yusufsefaartar@hotmail.com>
2026-05-29 14:12:11 +0300

    Now token attributes are public

--- Token.cs
+++ Token.cs
@@ -3,9 +3,9 @@ namespace SimpleCompiler;
 
 class Token
 {
-    int Line { get; set; }
-    string? Lex { get; set; }
-    TokenType Type { get; set; }
+    public int Line { get; set; }
+    public string? Lex { get; set; }
+    public TokenType Type { get; set; }
 
     public Token(int Line, string Lex, TokenType Type)
     {
