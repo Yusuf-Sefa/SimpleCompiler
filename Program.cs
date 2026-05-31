42917241b9a0393c2886969430122428e2e404ff
Yusuf-Sefa-Laptop <yusufsefaartar@hotmail.com>
2026-05-31 19:50:22 +0300

    Test source code added in Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,33 @@ static class Program
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        string sourceCode = @"
+            int x;
+            float y;
+            x = 10;
+            y = (x @ 5) * 2;
+            
+            if (x {
+                while (y) {
+                    print (5 + 3);
+                }
+            }
+        ";
+
+        try
+        {
+            Lexer lexer = new Lexer(sourceCode);
+            Parser parser = new Parser(lexer);
+            
+            parser.Parse();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n[DERLEME HATASI] {ex.Message}");
+            Console.ResetColor();
+        }
+
+        Console.ReadLine();
     }    
 }
